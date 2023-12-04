e8bd4b95d705ad0adfeb01b8e4a3a72ec18f14ea
amorphous1 <harrystew@gmail.com>
2023-12-04 15:52:46 +0000

    add day4 part2 solution

--- src/main/java/com/github/amorphous1/aoc2023/Day04.java
+++ src/main/java/com/github/amorphous1/aoc2023/Day04.java
@@ -4,14 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -19,10 +12,23 @@ public class Day04 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         String input = Files.readString(Paths.get(Day04.class.getClassLoader().getResource("day04.input").toURI()));
         System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 
     static int part1(String input) {
         return input.lines().map(Scratchcard::parse).map(Scratchcard::points).mapToInt(i -> i).sum();
+    }
+
+    static int part2(String input) {
+        List<Scratchcard> scratchcards = input.lines().map(Scratchcard::parse).toList();
+        Map<Integer, Integer> scratchCardCopiesById = new HashMap<>();
+        for (Scratchcard card : scratchcards) {
+            int totalNumberOfThisCard = 1 + scratchCardCopiesById.getOrDefault(card.id, 0);
+            for (int nextId = card.id + 1; nextId <= card.id + card.winningOwnNumbers() && nextId <= scratchcards.size(); nextId++) {
+                scratchCardCopiesById.put(nextId, scratchCardCopiesById.getOrDefault(nextId, 0) + totalNumberOfThisCard);
+            }
+        }
+        return scratchcards.size() + scratchCardCopiesById.values().stream().mapToInt(i -> i).sum();
     }
 
     private record Scratchcard(int id, Set<Integer> winningNumbers, Set<Integer> ownNumbers) {
@@ -35,10 +41,14 @@ public class Day04 {
             return new Scratchcard(id, winningNumbers, ownNumbers);
         }
 
-        public int points() {
+        public int winningOwnNumbers() {
             Set<Integer> winningOwnNumbers = new HashSet<>(ownNumbers);
             winningOwnNumbers.retainAll(winningNumbers);
-            return winningOwnNumbers.isEmpty() ? 0 : 1 << (winningOwnNumbers.size() - 1);
+            return winningOwnNumbers.size();
+        }
+
+        public int points() {
+            return winningOwnNumbers() == 0 ? 0 : 1 << (winningOwnNumbers() - 1);
         }
     }
 }
