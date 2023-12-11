16b5ec8ccb80cb0a7a29f08cb5aebdf9931cde4f
amorphous1 <harrystew@gmail.com>
2023-12-11 22:09:32 +0000

    refactor day09 to avoid duplication

--- src/main/java/com/github/amorphous1/aoc2023/Day09.java
+++ src/main/java/com/github/amorphous1/aoc2023/Day09.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class Day09 {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -13,31 +14,24 @@ public class Day09 {
         System.out.println(part2(input));
     }
     static long part1(final String input) {
-        return input.lines().mapToLong(line -> {
-            List<List<Integer>> deltas = calculateDeltas(line);
-            long extrapolated = 0;
-            for (int i = deltas.size() - 2; i >=0; i--)
-                extrapolated = deltas.get(i).getLast() + extrapolated;
-            return extrapolated;
-        }).sum();
+        return extrapolate(input, (deltas, prevPrediction) -> deltas.getLast() + prevPrediction);
     }
 
     static long part2(final String input) {
-        return input.lines().mapToLong(line -> {
-            List<List<Integer>> deltas = calculateDeltas(line);
-            long extrapolated = 0;
-            for (int i = deltas.size() - 2; i >=0; i--)
-                extrapolated = deltas.get(i).getFirst() - extrapolated;
-            return extrapolated;
-        }).sum();
+        return extrapolate(input, (deltas, prevPrediction) -> deltas.getFirst() - prevPrediction);
     }
 
-    private static List<List<Integer>> calculateDeltas(String line) {
-        List<Integer> numbers = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
-        List<List<Integer>> deltas = new ArrayList<>(List.of(numbers));
-        while (!deltas.getLast().stream().allMatch(d -> d == 0))
-            deltas.add(deltasFrom(deltas.getLast()));
-        return deltas;
+    private static long extrapolate(String input, BiFunction<List<Integer>, Long, Long> extrapolation) {
+        return input.lines().mapToLong(line -> {
+            List<Integer> numbers = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
+            List<List<Integer>> deltas = new ArrayList<>(List.of(numbers));
+            while (!deltas.getLast().stream().allMatch(d -> d == 0))
+                deltas.add(deltasFrom(deltas.getLast()));
+            long extrapolated = 0;
+            for (int i = deltas.size() - 2; i >= 0; i--)
+                extrapolated = extrapolation.apply(deltas.get(i), extrapolated);
+            return extrapolated;
+        }).sum();
     }
 
     private static List<Integer> deltasFrom(List<Integer> numbers) {
