e3027be741e23d0ca4f293d4b784fc1b0edb9267
amorphous1 <harrystew@gmail.com>
2023-12-22 17:39:25 +0000

    refactor day14 part1

--- src/main/java/com/github/amorphous1/aoc2023/Day14.java
+++ src/main/java/com/github/amorphous1/aoc2023/Day14.java
@@ -5,12 +5,8 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 public class Day14 {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -20,54 +16,45 @@ public class Day14 {
 
     static long part1(String input) {
         Platform platform = Platform.parse(input);
-        System.out.println(platform);
-        System.out.println("------------------------------------------");
         platform.tiltNorth();
-        System.out.println(platform);
         return platform.totalLoad();
     }
 
     record Platform(String[] rows) {
+        private static final Character EMPTY = '.';
+        private static final Character STATIC_ROCK = '#';
+        private static final Character MOVING_ROCK = 'O';
+
         public void tiltNorth() {
-            for (int col = 0; col < rows[0].length(); col++) {
-                int row = 0;
-                while (row < rows.length) {
-                    if (rows[row].charAt(col) == '.') { // empty space, can move rocks north
+            for (int col = 0; col < rows[0].length(); col++)
+                for (int row = 0; row < rows.length; row++)
+                    if (rows[row].charAt(col) == EMPTY) {
                         List<Integer> movingRocks = findMovingRocks(col, row);
                         for (int movingRockIdx : movingRocks) {
-                            rows[movingRockIdx] = rows[movingRockIdx].substring(0, col) + '.' + rows[movingRockIdx].substring(col + 1);
-                            rows[row] = rows[row].substring(0, col) + 'O' + rows[row].substring(col + 1);
+                            rows[movingRockIdx] = rows[movingRockIdx].substring(0, col) + EMPTY + rows[movingRockIdx].substring(col + 1);
+                            rows[row] = rows[row].substring(0, col) + MOVING_ROCK + rows[row].substring(col + 1);
                             row++;
                         }
-                        row = movingRocks.isEmpty() ? row + 1 : movingRocks.getLast() + 1;
-                    } else {
-                        row++;
+                        row = movingRocks.isEmpty() ? row : movingRocks.getLast();
                     }
-                }
-            }
         }
 
         public long totalLoad() {
             return IntStream.range(0, rows.length)
-                    .map(row -> rows[row].replaceAll("[.#]", "").length() * (rows.length - row))
+                    .map(row -> rows[row].replaceAll("[^" + MOVING_ROCK + "]", "").length() * (rows.length - row))
                     .sum();
         }
 
         private List<Integer> findMovingRocks(int col, int startRow) {
             List<Integer> result = new ArrayList<>();
-            for (int row = startRow; row < rows.length && rows[row].charAt(col) != '#'; row++)
-                if (rows[row].charAt(col) == 'O')
+            for (int row = startRow; row < rows.length && rows[row].charAt(col) != STATIC_ROCK; row++)
+                if (rows[row].charAt(col) == MOVING_ROCK)
                     result.add(row);
             return result;
         }
 
         static Platform parse(String input) {
             return new Platform(input.lines().toArray(String[]::new));
-        }
-
-        @Override
-        public String toString() {
-            return String.join("\n", rows);
         }
     }
 }
